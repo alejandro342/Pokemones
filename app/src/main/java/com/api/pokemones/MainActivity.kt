880e009a0a12973988181e334285ec121796b184
Alejandro Ambrosio <victor.ambrosiot@sbs21consultoria.mx>
2025-12-08 18:19:37 -0600

    Se corrigio el thema de la aplicación.

--- app/src/main/java/com/api/pokemones/MainActivity.kt
+++ app/src/main/java/com/api/pokemones/MainActivity.kt
@@ -3,7 +3,6 @@ package com.api.pokemones
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.api.pokemones.navhots.navigation.NavigationC
 import com.api.pokemones.presentation.ui.theme.PokemonesTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +12,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            PokemonesTheme {
                 NavigationC()
+            }
         }
     }
 }
